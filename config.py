d33ac40347f1e5a8671d1f4f1a7232d09b295c3f
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-03-28 22:38:34 +0200

    move `~/.sched` to `~/.config/sched`

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+from pathlib import Path
 import yaml
 
-SCHED_DIR = '/home/nicolai/.sched'
-CONFIG_FILE = SCHED_DIR + '/config.yml'
-SPOOL_FILE = SCHED_DIR + '/spool.yml'
+SCHED_DIR = Path.home() / '.config' / 'sched'
+CONFIG_FILE = SCHED_DIR / 'config.yml'
+SPOOL_FILE = SCHED_DIR / 'spool.yml'
 
 with open(CONFIG_FILE, 'r') as config_file:
     config = yaml.safe_load(config_file)
